b5b29ee1b2e361b473c14e4647dac4976de733fd
TechTropolis <officialtechtropolis@gmail.com>
2025-04-20 18:34:16 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // <== دا اللي بيفعّل الدارك مود لما نضيف class="dark"
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
